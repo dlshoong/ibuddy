cdb4843fc85311be2976c4074d298a5881dbbc4e
dlshoong <dlshoong@users.noreply.github.com>
2020-02-19 14:54:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+        try {
+      
+                echo "[{\"BRAND\":\"HERCEPTIN\",\"CURRENT_SUM_SALES\":\"18031601\"},{\"BRAND\":\"RECORMON\",\"CURRENT_SUM_SALES\":\"14622660\"},{\"BRAND\":\"ROCEPHIN\",\"CURRENT_SUM_SALES\":\"13606839\"},{\"BRAND\":\"AVASTIN\",\"CURRENT_SUM_SALES\":\"12569620\"},{\"BRAND\":\"CELLCEPT\",\"CURRENT_SUM_SALES\":\"11730195\"}]";
+     
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+
+   
+?>
