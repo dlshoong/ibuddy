6c311a728d2a725850c7225feed47b92d90436c5
dlshoong <dlshoong@users.noreply.github.com>
2020-02-19 15:28:14 +0800

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,12 @@
+<?php
+
+        try {
+      
+                echo "[{\"BRAND\":\"CELLCEPT\"},{\"BRAND\":\"MIRCERA\"},{\"BRAND\":\"RECORMON\"}]";
+     
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+
+   
+?>
